e3afcb9930af43ea358f0d435c74fcfc314d6d44
Matthias <83934411+mnigmann@users.noreply.github.com>
2022-04-21 02:30:07 -0400

    Added code to set chip select settings

--- analog_read.c
+++ analog_read.c
@@ -24,12 +24,13 @@
  * thermistor) by charging a capacitor through the unknown resistance
  * and measuring the amount of time it takes for the digital pin to read
  * the voltage on the capacitor as "high"
+ * csmode = {,,,,,cs,invert_input,cs_active_low}
  */
-double measure(int ch, uint32_t sampling_interval) {
+double measure(int ch, uint32_t sampling_interval, uint8_t csmode) {
     gpio_mode(11, 4);
     gpio_mode(8, 4);
     gpio_mode(9, 4);
-    *VIRT_SPI_REG(SPI_CS) = (1<<11) | (3<<4) | (1<<21);
+    *VIRT_SPI_REG(SPI_CS) = (1<<11) | (3<<4) | ((csmode & 1) ? 0 : (1<<21)) | ((csmode >> 2) & 1);
     uint32_t cs = *VIRT_SPI_REG(SPI_CS);
     *VIRT_SPI_REG(SPI_CLK) = (uint32_t)(CLOCK_KHZ / 512.0 * sampling_interval / 1000);
     usleep(100);
@@ -44,13 +45,13 @@ double measure(int ch, uint32_t sampling_interval) {
     uint32_t time_diff = 0;
     while (*VIRT_SPI_REG(SPI_CS) & (1<<17)) {
         val = *VIRT_SPI_REG(SPI_FIFO);
-        val = ~val;
+        if ((csmode & 3) == 0 || (csmode & 3) == 3) val = ~val;
         for (uint8_t i=0; i<8; i++) {
             time_diff += (val & 1);
             val = val >> 1;
         }
     }
-    gpio_out(9, 0);
+    gpio_out(9, csmode & 1);
     gpio_mode(9, 1);
     usleep(1000);
     gpio_mode(9, 4);
@@ -65,17 +66,27 @@ double measure(int ch, uint32_t sampling_interval) {
  * smapling interval, and a shorter interval means they are closer
  * together.
  */
-double measure_precise(int ch, uint32_t sampling_interval) {
-    double m1 = measure(ch, sampling_interval);
-    return measure(ch, m1*1.5);
+double measure_precise(int ch, uint32_t sampling_interval, uint8_t cspol) {
+    double m1 = measure(ch, sampling_interval, cspol);
+    return measure(ch, m1*1.5, cspol);
 }
 
+double measure_distance(int ch, uint32_t sampling_interval) {
+    gpio_pud(9, GPIO_PUD_UP);
+    double m_time = measure_precise(ch, sampling_interval, 3);
+    gpio_pud(9, GPIO_PUD_OFF);
+    return m_time * 343 / 2000000;
+}
+
+#ifdef MAIN_IS_ANALOG
 int main() {
     virt_gpio_regs = map_segment((void*)GPIO_BASE, PAGE_SIZE);
     virt_spi_regs = map_segment((void*)SPI_BASE, PAGE_SIZE);
 
-
-    double m_time = measure_precise(0, 4000);
+//    double dist = measure_distance(0, 15000);
+//    printf("Distance is %f\n", dist);
+    double m_time = measure_precise(0, 4000, 0);
+//    m_time = measure(0, 20000, 0);
     double res = (sqrt(MOD_B*MOD_B - 4*MOD_A*(MOD_C - m_time)) - MOD_B)/(2*MOD_A);
     printf("Time is %fus, estimated resistance is %f\n", m_time, res);
 
@@ -88,3 +99,4 @@ int main() {
 
     return 0;
 }
+#endif
