1ba0afa9eed35a772b22e2c0b32727bbb87c695e
Matthias <83934411+mnigmann@users.noreply.github.com>
2022-05-18 23:53:56 -0400

    Proper support of multiple channels

--- analog_read.c
+++ analog_read.c
@@ -28,9 +28,10 @@
  */
 double measure(int ch, uint32_t sampling_interval, uint8_t csmode) {
     gpio_mode(11, 4);
-    gpio_mode(8, 4);
+    /*if (csmode & 0b100)*/ gpio_mode(7, 4);
+    /*else*/ gpio_mode(8, 4);
     gpio_mode(9, 4);
-    *VIRT_SPI_REG(SPI_CS) = (1<<11) | (3<<4) | ((csmode & 1) ? 0 : (1<<21)) | ((csmode >> 2) & 1);
+    *VIRT_SPI_REG(SPI_CS) = (1<<11) | (3<<4) | ((csmode & 1) ? 0 : (3<<21)) | ((csmode >> 2) & 1);
     uint32_t cs = *VIRT_SPI_REG(SPI_CS);
     *VIRT_SPI_REG(SPI_CLK) = (uint32_t)(CLOCK_KHZ / 512.0 * sampling_interval / 1000);
     usleep(100);
@@ -45,6 +46,9 @@ double measure(int ch, uint32_t sampling_interval, uint8_t csmode) {
     uint32_t time_diff = 0;
     while (*VIRT_SPI_REG(SPI_CS) & (1<<17)) {
         val = *VIRT_SPI_REG(SPI_FIFO);
+#if DEBUG
+        printf("%02X ", val);
+#endif
         if ((csmode & 3) == 0 || (csmode & 3) == 3) val = ~val;
         for (uint8_t i=0; i<8; i++) {
             time_diff += (val & 1);
@@ -55,6 +59,9 @@ double measure(int ch, uint32_t sampling_interval, uint8_t csmode) {
     gpio_mode(9, 1);
     usleep(1000);
     gpio_mode(9, 4);
+#if DEBUG
+    printf("\n");
+#endif
     return time_diff / 512.0 * sampling_interval;
 }
 
@@ -68,6 +75,7 @@ double measure(int ch, uint32_t sampling_interval, uint8_t csmode) {
  */
 double measure_precise(int ch, uint32_t sampling_interval, uint8_t cspol) {
     double m1 = measure(ch, sampling_interval, cspol);
+    if (m1 == 0) m1 = 50;
     return measure(ch, m1*1.5, cspol);
 }
 
@@ -79,13 +87,16 @@ double measure_distance(int ch, uint32_t sampling_interval) {
 }
 
 #ifdef MAIN_IS_ANALOG
-int main() {
+int main(int argc, char **argv) {
     virt_gpio_regs = map_segment((void*)GPIO_BASE, PAGE_SIZE);
     virt_spi_regs = map_segment((void*)SPI_BASE, PAGE_SIZE);
 
 //    double dist = measure_distance(0, 15000);
 //    printf("Distance is %f\n", dist);
-    double m_time = measure_precise(0, 4000, 0);
+    uint8_t ch = 0;
+    if (argc > 1 && strcmp(argv[1], "1") == 0) ch = 0b100;
+    printf("csmode %d, argv >%s<\n", ch, argv[1]);
+    double m_time = measure_precise(0, 10000, ch);
 //    m_time = measure(0, 20000, 0);
     double res = (sqrt(MOD_B*MOD_B - 4*MOD_A*(MOD_C - m_time)) - MOD_B)/(2*MOD_A);
     printf("Time is %fus, estimated resistance is %f\n", m_time, res);
